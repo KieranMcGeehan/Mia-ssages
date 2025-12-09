a651a451271febd2a384b177563f2113e91a60bc
Kieran McGeehan <167707711+KieranMcGeehan@users.noreply.github.com>
2025-12-09 20:43:35 +0100

    Change food to 11

--- timers.py
+++ timers.py
@@ -4,7 +4,7 @@ from timer_list import TimerList
 
 def define_timers(timers: TimerList):
     timers.daily("08:00", MiassageModes.EYES_TABLET_SUPPLIMENT)
-    timers.daily("08:00", MiassageModes.FOOD, seconds_offset=1)
+    timers.daily("11:00", MiassageModes.FOOD)
     timers.daily("13:00", MiassageModes.EYES)
     timers.daily("18:00", MiassageModes.EYES_TABLET)
     timers.daily("18:00", MiassageModes.FOOD, seconds_offset=1)
